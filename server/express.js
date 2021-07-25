0fb6110553ef99f40d16448c23f7c6ebdc3e746a
DESKTOP-K3EE9PG\NMC <minhchiendc@gmail.com>
2021-07-25 15:47:29 +0700

    update express

--- server/express.js
+++ server/express.js
@@ -14,6 +14,7 @@ app.use(compress())
 app.use(helmet())
 app.use(cors())
 app.use('/', auth)
+//Auth error handling for express-jwt
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
         res.status(401).json({ "error": err.name + ": " + err.message })
